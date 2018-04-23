f9e7a26498a4f33e5d8186b60a5b92e5c825f343
Jihye Lee <psycho5cafe@gmail.com>
2018-04-23 18:32:42 +0900

    Update ClassifierChain_sample_code.py

--- ClassifierChain_sample_code.py
+++ ClassifierChain_sample_code.py
@@ -1,9 +1,6 @@
 
 # coding: utf-8
 
-# In[19]:
-
-
 #https://tensorflow.blog/2018/02/16/%EB%B6%84%EB%A5%98%EA%B8%B0-%EC%B2%B4%EC%9D%B8-classifierchain/
 #https://github.com/rickiepark/introduction_to_ml_with_python/blob/master/ClassifierChain.ipynb
 
@@ -26,9 +23,6 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2,
 #train_test_split: http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
 
 
-# In[20]:
-
-
 #로지스틱 회귀 
 from sklearn.linear_model import LogisticRegression
 from sklearn.multiclass import OneVsRestClassifier
@@ -38,15 +32,9 @@ ovr.fit(X_train, Y_train)
 pred_ovr = ovr.predict(X_test)
 
 
-# In[5]:
-
-
 from sklearn.metrics import jaccard_similarity_score
 ovr_score = jaccard_similarity_score(Y_test, pred_ovr)
 ovr_score
-
-
-# In[6]:
 
 
 from sklearn.multioutput import ClassifierChain
@@ -58,8 +46,6 @@ cc_score = jaccard_similarity_score(Y_test, pred_cc)
 cc_score
 
 
-# In[7]:
-
 
 chains = [ClassifierChain(LogisticRegression(), order='random', random_state=42+i)
           for i in range(10)]
@@ -69,9 +55,6 @@ for chain in chains:
 pred_chains = np.array([chain.predict(X_test) for chain in chains])
 chain_scores = [jaccard_similarity_score(Y_test, pred_chain)
                     for pred_chain in pred_chains]
-
-
-# In[8]:
 
 
 proba_chains = np.array([chain.predict_proba(X_test) for chain in chains])
